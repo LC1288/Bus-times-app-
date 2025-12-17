c5cde20bfb44b20ea972f69e29fb56a6d10b8228
LC1288 <LC1288@student.aru.ac.uk>
2025-12-17 16:56:40 +0000

    First Save

---
+++ Start.js
@@ -0,0 +1,10 @@
+WebGL2RenderingContext() {
+    const userCount =_.multiply(this.state.userCount,100)
+
+    return(
+        <header className="Site-header">
+
+        <span className="pitch">Toast as a service</span>
+        </header>
+    );
+}
